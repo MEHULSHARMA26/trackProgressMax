a768ef70f4b05a49b5f2fb2b70c6f61e4665d56e
MEHULSHARMA26 <sharmaamehul26@gmail.com>
2024-11-17 17:21:01 +0530

    these are JS concepts!

---
+++ JS_CONCEPTS/IntroToJS/script.js
@@ -0,0 +1,18 @@
+// alert("hello world!");
+
+console.log("Code is running!");
+
+// var a = prompt("Enter your number!");
+// console.log("your number is "+a);
+
+// var isTrue = confirm("are your sure you want to leave this page and kaboom the computer?");
+
+// if(isTrue){
+//     console.log("your computer is successfully kaboomed!")
+// }
+// else{
+//     console.log("Your computer ain't boom boom!");
+// }
+
+//NodeJs is javacript runtime environment 
+///nodeJs => javascript a V8 engine from browser injected in c++ which made possible to run javascript program on client system without browser
---
+++ JS_CONCEPTS/variables/script.js
@@ -0,0 +1,22 @@
+console.log("hey this is tutorial 55!");
+
+//var keyword
+
+// var a =5;
+// var b= 6;
+// var c = "maximal";
+let a= 5;
+a =a+1;
+let b= 9;
+let c="maximal";
+
+console.log(a+b+9);
+{
+    let a =9999;
+    console.log(a);
+}
+console.log(typeof a,typeof b,typeof c);
+
+//we should use 'let' to make variables instead of 
+
+/* var is global scoped while let & const are block scoped*/
